a32aeacd0d215eae3e2627cbbf160282fe10df06
sthi-1005 <79720695+sthi-1005@users.noreply.github.com>
2021-08-01 19:31:17 -0600

    upload analysis script
    contains script for MPG and Coil analysis

---
+++ MechaCarChallenge.R
@@ -0,0 +1,9 @@
+library(dplyr)
+mpg<-read.csv(file='MechaCar_mpg.csv')
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg)
+summary_mpg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg) %>% summary()
+
+library(tidyverse)
+coil<-read.csv(file='Suspension_Coil.csv')
+total_summary<-coil%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary<-coil%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
