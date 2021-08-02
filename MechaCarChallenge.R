6164de8724c0710591a4b6340cba5942cc79a2e5
sthi-1005 <79720695+sthi-1005@users.noreply.github.com>
2021-08-01 20:34:10 -0600

    updated r script for suspension coil
    summary stats and ttest

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,8 @@ library(tidyverse)
 coil<-read.csv(file='Suspension_Coil.csv')
 total_summary<-coil%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary<-coil%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+t.test(coil$PSI,mu=1500)
+t.test(subset(coil,subset=Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+t.test(subset(coil,subset=Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+t.test(subset(coil,subset=Manufacturing_Lot=="Lot3")$PSI,mu=1500)
